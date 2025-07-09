636a098978e846e21fa405dd8e9ec2e740a9de6e
asad <aurehman.bese21seecs@seecs.edu.pk>
2025-07-09 11:43:09 +0500

    resolved the delete hhhh controller and app.service files issues and the firebase-service-account now importing from env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TasksModule } from './modules/tasks/tasks.module';
 import { UserModule } from './modules/users/users.module';
 
@@ -14,7 +12,7 @@ import { UserModule } from './modules/users/users.module';
     TasksModule,
     UserModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
